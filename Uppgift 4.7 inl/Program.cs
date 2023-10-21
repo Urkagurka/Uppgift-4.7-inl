a662526d84c3abd2e08a8cbe7364e2367b214631
Urkagurka <143172181+Urkagurka@users.noreply.github.com>
2023-10-21 17:10:59 +0200

    Add project files.

---
+++ Uppgift 4.7 inl/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Uppgift_4._7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in en text");
+            string text = Console.ReadLine();
+            Console.WriteLine("Skriv in ett heltal");
+            int rum = int.Parse(Console.ReadLine());
+            for (int i = 0; i < text.Length; i++)
+            {
+                string bokstav = text[i].ToString();
+                for (int j = 0; j < rum; j++)
+                {
+                    Console.Write(" ");
+                }
+                Console.WriteLine(bokstav);
+            }
+        }
+    }
+}
